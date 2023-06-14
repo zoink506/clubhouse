bb342d9e0c4b3d53336c045f98a70357181a6342
zoink506 <benhubble03@gmail.com>
2023-06-14 18:50:46 +1000

    Create index page for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+  get 'home/index'
+  resources :posts, only: [:index]
 
   root to: "home#index"
 end
